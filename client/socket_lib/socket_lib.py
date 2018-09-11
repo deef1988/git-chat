83f2a70730f23410adad80d6cb125f14a963716e
def1988 <def1988@inbox.ru>
2018-09-11 16:22:34 +0300

    изменен способ отправки сообщений

--- client/socket_lib/socket_lib.py
+++ client/socket_lib/socket_lib.py
@@ -1,6 +1,9 @@
+import sys
+sys.path.append("..")
 import datetime
-import select
 from socket import socket, AF_INET, SOCK_STREAM
+from message.message_lib import Type_message
+
 import json
 
 class Socket_client_send():
@@ -13,48 +16,52 @@ class Socket_client_send():
         self.s = socket(AF_INET, SOCK_STREAM)
         self.s.connect((addres, port))
         self.connect = False
-        # self.name = None
 
     def __delete__(self, instance):
         self.s.close()
 
+    def _mess_send(self, message):
+        message_json = json.dumps(message)
+        message_buf = message_json.encode('utf-8')
+        self.s.send(message_buf)
+        pass
+    def _mess_get(self):
+        return mess
+        pass
+
     def connect_server(self, *args, **kwargs):
-        # print('args',args)
-        # print('kwargs',kwargs)
-        print('start connect')
         self.name = kwargs['name']
         self.passwd = kwargs['passwd']
-        message = {
-            "action": "authenticate",
-            "time": str(datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")),
-            "user": {
-                "account_name": self.name,
-                "password": self.passwd,
-            },
-        }
-        message_json = json.dumps(message)
-        message_buf = message_json.encode('utf-8')
-        self.s.send(message_buf)
-        print(message_buf)
+        # message = {
+        #     "action": "authenticate",
+        #     "time": str(datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")),
+        #     "user": {
+        #         "account_name": self.name,
+        #         "password": self.passwd,
+        #     },
+        # }
+        # message_json = json.dumps(message)
+        # message_buf = message_json.encode('utf-8')
+        # self.s.send(message_buf)
+        self._mess_send(Type_message.authenticate(name=self.name, passed=self.passwd, id_mess=1))#id_mess надо брать из БД
+        # print(message_buf)
         return 'send connect'
 
-    def send_mess(self, *args, **kwargs ):
-        nameto = kwargs['nameto']
-        msg = kwargs['msg']
-        id_mess = kwargs['id_mess']
-        # nameto = input('для кого сообщение(all для всех)')
-        # msg = input('введите сообзение')
-        message = {
-            "action": "msg",
-            "time": str(datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")),
-            "to": nameto,
-            "from": self.name,
-            "id_mess": id_mess,
-            "encoding": "utf-8",
-            "message": msg,
-        }
-        print('Попытка отправки')
-        message_json = json.dumps(message)
-        message_buf = message_json.encode('utf-8')
-        self.s.send(message_buf)
-        print(message_buf)
+    # def send_mess(self, *args, **kwargs ):
+    #     nameto = kwargs['nameto']
+    #     msg = kwargs['msg']
+    #     id_mess = kwargs['id_mess']
+    #     message = {
+    #         "action": "msg",
+    #         "time": str(datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")),
+    #         "to": nameto,
+    #         "from": self.name,
+    #         "id_mess": id_mess,
+    #         "encoding": "utf-8",
+    #         "message": msg,
+    #     }
+    #     print('Попытка отправки')
+    #     message_json = json.dumps(message)
+    #     message_buf = message_json.encode('utf-8')
+    #     self.s.send(message_buf)
+    #     print(message_buf)
