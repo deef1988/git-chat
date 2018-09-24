e6c16188e86ce331618ac5aea2357aba2af4901b
def1988 <def1988@inbox.ru>
2018-09-24 14:56:39 +0300

    Переделана функция соединения с сервером, сообщения перед передачей заносится в БД, но еще не отправляется на сервер

--- client/socket_lib/socket_lib.py
+++ client/socket_lib/socket_lib.py
@@ -3,6 +3,7 @@ sys.path.append("..")
 import datetime
 from socket import socket, AF_INET, SOCK_STREAM
 from message.message_lib import Type_message
+from db.db_Alchemy import db_Alchimy as db
 
 import json
 
@@ -16,6 +17,7 @@ class Socket_client_send():
         self.s = socket(AF_INET, SOCK_STREAM)
         self.s.connect((addres, port))
         self.connect = False
+        self.session = db.init_db()
 
     def __delete__(self, instance):
         self.s.close()
@@ -29,23 +31,18 @@ class Socket_client_send():
         return mess
         pass
 
+    def _message_buf(self, message):
+        message_json = json.dumps(message)
+        message_buf = message_json.encode('utf-8')
+        return message_buf
+
     def connect_server(self, *args, **kwargs):
         self.name = kwargs['name']
         self.passwd = kwargs['passwd']
-        # message = {
-        #     "action": "authenticate",
-        #     "time": str(datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")),
-        #     "user": {
-        #         "account_name": self.name,
-        #         "password": self.passwd,
-        #     },
-        # }
-        # message_json = json.dumps(message)
-        # message_buf = message_json.encode('utf-8')
-        # self.s.send(message_buf)
         #записываем в БД сообщение на авторизацию
-        self._mess_send(Type_message.authenticate(name=self.name, passed=self.passwd, id_mess=1))#id_mess надо брать из БД
-        # print(message_buf)
+        message = Type_message.authenticate(name=self.name, passed=self.passwd, id_mess=1)
+        db.add_mess_send(self.session,  id_user_to = self.name, id_user_from = '', mess = self._message_buf(message), status_mess = 's')
+        # self._mess_send(Type_message.authenticate(name=self.name, passed=self.passwd, id_mess=1))#id_mess надо брать из БД
         return 'send connect'
 
     # def send_mess(self, *args, **kwargs ):
